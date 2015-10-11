b899b5a6c28312eeb08e66550fa1235b703c64c9
pkafel <piotr.kafel@gmail.com>
2015-10-11 19:38:57 +0200

    Solution for exercise 3.29

--- src/main/scala/com/piotrkafel/fpis/Chapter3.scala
+++ src/main/scala/com/piotrkafel/fpis/Chapter3.scala
@@ -172,32 +172,29 @@ object Chapter3 {
   /**
    * Implementation for exercise 3.25
    */
-  def size[A](t: Tree[A]): Int = t match {
-    case Leaf(_) => 1
-    case Branch(l,r) => size(l) + size(r) + 1
-  }
+  def size[A](t: Tree[A]): Int = fold(t) (_ => 1) ((l,r) => l + r + 1)
 
   /**
    * Implementation for exercise 3.26
    */
-  def maximum(t: Tree[Int]): Int = t match {
-    case Leaf(v) => v
-    case Branch(l,r) => maximum(l).max(maximum(r))
-  }
+  def maximum(t: Tree[Int]): Int = fold(t) (v => v) ((l,r) => l.max(r))
 
   /**
    * Implementation for exercise 3.27
    */
-  def depth[A](t: Tree[A]): Int = t match {
-    case Leaf(_) => 1
-    case Branch(l,r) => depth(l).max(depth(r)) + 1
-  }
+  def depth[A](t: Tree[A]): Int = fold(t) (_ => 1) ((l,r) => l.max(r) + 1)
 
   /**
    * Implementation for exercise 3.28
    */
-  def treeMap[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
-    case Leaf(v) => Leaf(f(v))
-    case Branch(l,r) => Branch(treeMap(l)(f), treeMap(r)(f))
+  def treeMap[A, B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t) (v => Leaf(f(v)): Tree[B]) ((l,r) => Branch(l, r))
+
+  /**
+   * Implementation for exercise 3.29
+   */
+  def fold[A,B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(v) => f(v)
+    case Branch(l,r) => g(fold(l)(f)(g),fold(r)(f)(g))
   }
 }
